77bdd267438e3da80092bc6b8df34d7816bc071a
RayaneRahmatErrahmaneSMARA <mr_smara@esi.dz>
2025-03-14 11:00:00 +0100

    SideBar responsive

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,14 +2,9 @@ import SideBar from "../app/components/SideBar/SideBar";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex min-h-screen bg-[rgb(255,255,255)]">
       <SideBar />
-      <div className="flex-1 p-6">
-        <h1 className="text-3xl font-bold">Welcome to My Next.js App!</h1>
-        <p className="text-gray-600 mt-2">
-          This is a simple test to check if the Sidebar is working.
-        </p>
-      </div>
+     
     </div>
   );
 }
